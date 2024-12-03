16e9118692de9a817a3f71df1dcbcf8d0b114eb7
gladius240 <121557815+GLADIUS240@users.noreply.github.com>
2024-12-03 23:20:42 +0530

    Added Category menu in the product page or component with category filering and queryparam for url

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,18 +1,41 @@
 import { Component } from '@angular/core';
 import { ProductService } from '../product.service';
-import { CommonModule, NgFor } from '@angular/common';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
+import { CategoryService } from '../category.service';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Product } from '../models/product';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CommonModule,NgFor],
+  imports: [CommonModule,NgFor,NgIf,RouterLink],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
 
-  products$: any;
-  constructor(private productService:ProductService){
-    this.products$= productService.getProducts();
+  products: Product[]=[];
+  filteredProducts:Product[]=[];
+  categories$: any;
+  category!: String | null;
+
+  constructor(
+    route:ActivatedRoute,
+    private productService:ProductService, 
+    categoryService:CategoryService){
+    productService.getProducts().subscribe(products=>{
+      this.products=products;
+
+      route.queryParamMap.subscribe(params=>{
+        this.category=params.get('category');
+  
+        this.filteredProducts=(this.category)?
+        this.products.filter(p=>p.category===this.category):
+        this.products;
+      });
+    });
+    this.categories$=categoryService.getCategories();
+
+   
   }
 }
